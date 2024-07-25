2a579cfdb17559239fbc2157506f79e557afae9b
PPUDANI <zxvk1030@naver.com>
2024-07-25 17:26:04 +0900

    1845 문제 풀이

---
+++ JDK_CodingStudy/1845/1845.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    // 중복이 없고 정렬하지 않는 unordered_set을 사용
+    std::unordered_set<int> Ponketmons(nums.begin(), nums.end());
+    
+    int MaxNumberOfPick = static_cast<int>(nums.size()) / 2;
+    int NumberOfPonketmon = static_cast<int>(Ponketmons.size());
+
+    return MaxNumberOfPick < NumberOfPonketmon ? MaxNumberOfPick : NumberOfPonketmon;
+}   
