f0558641142730e817d436f987ce568846309c7b
PPUDANI <zxvk1030@naver.com>
2024-07-26 00:08:51 +0900

    134240 수정

--- JDK_CodingStudy/134240/134240.cpp
+++ JDK_CodingStudy/134240/134240.cpp
@@ -8,22 +8,21 @@ string solution(vector<int> food)
 
     int NumberOfFoodTypes = static_cast<int>(food.size());
 
+    // Food 별 string 저장할 컨테이너
     std::vector<std::string> Foods(NumberOfFoodTypes, "");
 
-
+    // 순회하며 Food string을 만들고 answer에 추가
     for (int i = 0; i < NumberOfFoodTypes; ++i)
     {
         int NumberOfFood = food[i] / 2;
-        Foods[i].assign(NumberOfFood, i + '0');
-    }
-
-    for (int i = 0; i < NumberOfFoodTypes; ++i)
-    {
+        Foods[i].assign(NumberOfFood, i + '0'); 
         answer += Foods[i];
     }
 
+    // 0 추가
     answer += '0';
 
+    // 역순으로 answer에 추가
     for (int i = NumberOfFoodTypes - 1; i >= 0; --i)
     {
         answer += Foods[i];
