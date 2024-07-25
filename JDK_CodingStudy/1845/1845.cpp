5f80624e2d963c49a17d5f319aa0424bfddc362d
PPUDANI <zxvk1030@naver.com>
2024-07-26 00:20:02 +0900

    4주차 주석 추가

--- JDK_CodingStudy/1845/1845.cpp
+++ JDK_CodingStudy/1845/1845.cpp
@@ -7,8 +7,13 @@ int solution(vector<int> nums)
     // 중복이 없고 정렬하지 않는 unordered_set을 사용
     std::unordered_set<int> Ponketmons(nums.begin(), nums.end());
     
+    // 폰켓몬을 가져갈 수 있는 최대 개수 N/2
     int MaxNumberOfPick = static_cast<int>(nums.size()) / 2;
+
+    // 폰켓몬 종류의 개수
     int NumberOfPonketmon = static_cast<int>(Ponketmons.size());
 
+    // 폰켓몬 종류가 가져갈 수 있는 최대 개수보다 많으면 최대개수 리턴
+    // 최대 선택 개수가 폰켓몬 종류보다 많으면 폰켓몬 종류 리턴
     return MaxNumberOfPick < NumberOfPonketmon ? MaxNumberOfPick : NumberOfPonketmon;
 }   
