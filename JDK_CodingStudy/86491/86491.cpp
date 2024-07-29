d4da0238df476a68962be1b07763c344a6335d1f
PPUDANI <zxvk1030@naver.com>
2024-07-29 10:00:36 +0900

    5주차 업데이트

---
+++ JDK_CodingStudy/86491/86491.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes)
+{
+
+    int NumOfValue = static_cast<int>(sizes.size());
+    int MinSize = 0;
+    int MaxSize = 0;
+
+    for (int i = 0; i < NumOfValue; ++i)
+    {
+        int CurMin = std::min(sizes[i][0], sizes[i][1]);
+        int CurMax = std::max(sizes[i][0], sizes[i][1]);
+        MinSize = std::max(MinSize, CurMin);
+        MaxSize = std::max(MaxSize, CurMax);
+    }
+
+    int answer = MinSize * MaxSize;
+    return answer;
+}
