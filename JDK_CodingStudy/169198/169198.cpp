6f7c5b9187ce3f937aeeb73fab1fc49273901a91
PPUDANI <zxvk1030@naver.com>
2024-10-03 15:50:52 +0900

    10월 1주차 업데이트

---
+++ JDK_CodingStudy/169198/169198.cpp
@@ -0,0 +1,140 @@
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+class int4
+{
+public:
+    int4(int _Left = 0, int _Right = 0, int _Up = 0, int _Down = 0)
+        :Left(_Left), Right(_Right), Up(_Up), Down(_Down) 
+    {
+    }
+
+    union
+    {
+        int LRUD[4];
+        struct
+        {
+            int Left;
+            int Right;
+            int Up;
+            int Down;
+        };
+    };
+
+    int4 operator +(const int4& _RValue) const
+    {
+        int4 Res;
+        Res.Left = Left + _RValue.Left;
+        Res.Right = Right + _RValue.Right;
+        Res.Up = Up + _RValue.Up;
+        Res.Down = Down + _RValue.Down;
+        return Res;
+    }
+};
+
+vector<int> solution(int m, int n, int startX, int startY, vector<vector<int>> balls)
+{
+    vector<int> answer;
+
+    // 흰 공의 위치와 4방향의 쿠션과의 거리값
+    int4 Start(startX, m - startX, n - startY, startY);
+    for (vector<int> Ball : balls)
+    {
+        // Target의 위치와 4방향의 쿠션과의 거리값
+        int4 Target(Ball[0], m - Ball[0], n - Ball[1], Ball[1]);
+
+        // Start와 Target의 한 축이 겹친다면 갈 수 없는 방향 체크
+        int ImmovableCushionDir = -1;
+        if (startX == Ball[0])
+        {
+            ImmovableCushionDir = startY > Ball[1] ? 3 : 2;
+        }
+        else if(startY == Ball[1])
+        {
+            ImmovableCushionDir = startX > Ball[0] ? 0 : 1;
+        }
+
+        // 갈 수 없는 방향을 제외한 나머지 방향의 거리를 구하여 최소값 색출
+        int4 Sum = Start + Target;
+        int MinDistance = 2147483647;
+        for (int i = 0; i < 4; ++i)
+        {
+            if (i == ImmovableCushionDir)
+            {
+                continue;
+            }
+            
+            int StartToTarget = 0;
+			switch (i)
+			{
+			case 0: case 1:
+				StartToTarget = abs(startY - Ball[1]);
+				break;
+			case 2: case 3:
+				StartToTarget = abs(startX - Ball[0]);
+				break;
+			}
+
+            int Result = pow(Sum.LRUD[i], 2) + pow(StartToTarget, 2);
+            MinDistance = min(Result, MinDistance);
+        }
+
+        answer.push_back(MinDistance);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> Res = solution(10, 10, 3, 7, { { 7, 7 }, { 2, 7 }, { 7, 3 } });
+    vector<int> Res2 = solution(10, 10, 5, 9, {{ 5, 8 }});
+    vector<int> Res3 = solution(3, 3, 1, 1, { { 1, 2 } });
+    vector<int> Res4 = solution(10, 10, 1, 1, { { 1, 9 } });
+    vector<int> Res5 = solution(10, 10, 1, 9, { { 1, 8 } });
+}
+
+//vector<int> solution(int m, int n, int startX, int startY, vector<vector<int>> balls)
+//{
+//    vector<int> answer;
+//    int4 Start(startX, m - startX, n - startY, startY);
+//    
+//    for (vector<int> Ball : balls)
+//    {
+//        int4 Target(Ball[0], m - Ball[0], n - Ball[1], Ball[1]);
+//        int4 Sum = Start + Target;
+//
+//        int MinIndex = 0;
+//        int MinOfSum = 0;
+//
+//        bool Same_X = Ball[0] == startX;
+//        bool Same_Y = Ball[1] == startY;
+//
+//        for (int i = 0; i < 4; ++i)
+//        {
+//            if ()
+//            {
+//
+//            }
+//        }
+//
+//        int StartToTarget = 0;
+//        switch (MinIndex)
+//        {
+//        case 0: case 1:
+//            StartToTarget = abs(startY - Ball[1]);
+//            break;
+//
+//        case 2: case 3:
+//            StartToTarget = abs(startX - Ball[0]);
+//            break;
+//        }
+//
+//        int Result = pow(Sum.LRUD[MinIndex], 2) + pow(StartToTarget, 2);
+//        answer.push_back(Result);
+//    }
+//
+//    return answer;
+//}
---
+++ JDK_CodingStudy/72411/72411.cpp
@@ -0,0 +1,115 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+// 재귀로 넘기는 Parameter를 최소화 하기위해 Class로 제작함.
+class Combination_Str
+{
+public:
+    Combination_Str(const string& _Eliments, const int _CombiLength)
+        : Eliments(_Eliments), CombiLength(_CombiLength)
+    {
+        // 생성 시 문자열을 오름차순으로 정렬
+        sort(Eliments.begin(), Eliments.end(), [](const char _Left, const char _Right)
+            {
+                return _Left < _Right;
+            });
+
+        NumOfElement = static_cast<int>(Eliments.length());
+    }
+
+    //가장 많이 중복된 조합의 개수 리턴
+    inline int GetNumOfMostCombi() const
+    {
+        return NumOfMostCombi;
+    }
+    
+    void GetAllCombination(int _StartIndex, string _CurCombi, unordered_map<string, int>& _Combis)
+    {
+        int _DigitCount = CombiLength - 1;
+        CalculateCombination(_StartIndex, _DigitCount, _CurCombi, _Combis);
+    }
+
+    // 재귀 함수
+    void CalculateCombination(int _StartIndex, int _DigitCount, string _CurCombi, unordered_map<string, int>& _Combis)
+    {
+        if (_CurCombi.length() == CombiLength)
+        {
+            int NumberOfCurStr = ++_Combis[_CurCombi];
+            NumOfMostCombi = max(NumOfMostCombi, NumberOfCurStr);
+            return;
+        }
+
+        if (_DigitCount > 0)
+        {
+            --_DigitCount;
+        }
+
+        for (int i = _StartIndex; i < NumOfElement - _DigitCount; ++i)
+        {
+            CalculateCombination(i + 1, _DigitCount, _CurCombi + Eliments[i], _Combis);
+        }
+
+        return;
+    }
+
+private:
+    string Eliments;
+    int NumOfElement;
+
+    int CombiLength;
+    int NumOfMostCombi = 0;
+};
+
+
+vector<string> solution(vector<string> orders, vector<int> course) 
+{
+    vector<string> answer;
+
+    for (int CourseLength : course)
+    {
+        unordered_map<string, int> CourseMap;
+        int NumOfMostCombi = 0;
+
+        // 모든 코스 생성.
+        for (const string& Order : orders)
+        {
+            Combination_Str CombiInst(Order, CourseLength);
+            CombiInst.GetAllCombination(0, "", CourseMap);
+            NumOfMostCombi = max(NumOfMostCombi, CombiInst.GetNumOfMostCombi());
+        }
+
+        // 가장 많이 중복된 조합의 개수가 1이라면 continue.
+        if (NumOfMostCombi == 1)
+        {
+            continue;
+        }
+
+        // 가장 많이은 조합의 개수와 일치하면 적재
+        for (const pair<string, int>& Course : CourseMap)
+        {
+            if (Course.second == NumOfMostCombi)
+            {
+                answer.push_back(Course.first);
+            }
+        }
+    }
+
+    // answer 정렬
+    sort(answer.begin(), answer.end(), [](const string& _Left, const string& _Right)
+        {
+            return _Left < _Right;
+        });
+
+    return answer;
+}
+
+int main()
+{
+    vector<string> Res = solution({ "ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH" }, { 2, 3, 4 });
+    //vector<string> Res = solution({ "XYZ", "XWY", "WXA" }, { 2, 3, 4 });
+    int a = 0;
+}
